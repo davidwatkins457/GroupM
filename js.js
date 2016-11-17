ddc613bf8308f0a406cdd518df632db8571cb66c
Dallascat70 <alyssa.prah123@west-mec.org>
2016-11-17 14:36:12 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+window.onload(generateFaces());
+
+function generateFaces() {
+
+    document.createElement(img.src = "Doge.png");
+
+
+
+}
+
+
+
+
+var numberOfFaces = n=5;
+var theLeftSide = document.getElementById("leftSide");
+
+
+
+
